68472f529db07f2b1e5205ba44a078bba060e122
Your Name <you@example.com>
2022-01-25 17:34:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="index-login">
+        <div class="wrapper">
+            <div class="index-login-signup">
+                <h4>SIGN UP</h4>
+                <P>Dont't have an account yet? sign here!</P>
+                <form action="includes/signup.inc.php" method="POST">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <input type="password" name="pwdrepeat" placeholder="Repeat Password">
+                    <input type="text" name="email" placeholder="E-mail">
+                    <br>
+                    <button type="submit" name="submit">SIGN UP</button>
+                </form>
+            </div>
+            <div class="index-login-login">
+                <h4>LOGIN</h4>
+                <P>Dont't have an account yet? sign here!</P>
+                <form action="includes/login.inc.php" method="POST">
+                    <input type="text" name="uid" placeholder="Username">
+                    <input type="password" name="pwd" placeholder="Password">
+                    <br>
+                    <button type="submit" name="submit">LOGIN</button>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
